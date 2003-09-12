759a299b971cc9d036a3ef6e72a4de9801351297
keesj <keesj@03485ba7-651f-0410-bc53-9ddc93d75666>
2003-09-12 12:09:47 +0000

    added .png as a pass parameter
    git-svn-id: svn+ssh://localhost/home/svn/trunk@440 03485ba7-651f-0410-bc53-9ddc93d75666

--- src/te/jsp/JSPComponent.java
+++ src/te/jsp/JSPComponent.java
@@ -52,7 +52,7 @@ public class JSPComponent extends AbstractContainer implements Component {
      * @see te.Template#renderRelative(java.lang.String, te.WhiteBoard, java.io.PrintWriter)
      */
     public void renderRelative(String path, WhiteBoard wb) throws ServletException, IOException {
-        if (path.endsWith(".jsp") || path.endsWith(".jpg") || path.endsWith(".css") || path.endsWith(".css")){
+        if (path.endsWith(".jsp") || path.endsWith(".jpg") || path.endsWith(".css") || path.endsWith(".png") ){
 			mapRenderRelativeToRender = false;
         } else {
         	mapRenderRelativeToRender = true;
@@ -63,6 +63,7 @@ public class JSPComponent extends AbstractContainer implements Component {
             render(wb, wb.getHttpServletResponse().getWriter());
         } else {
 			log.debug("dispatching " + path );
+			
             int index = this.path.lastIndexOf(NavigationControl.PATH_SEPARATOR);
             if (index != -1) {
                 String realPath = this.path.substring(0, index);
