a53aa2bf5d1eeb0ebe1f17f4e002e05864d092d2
keesj <keesj@03485ba7-651f-0410-bc53-9ddc93d75666>
2003-10-29 16:21:23 +0000

    added a servlet variant
    git-svn-id: svn+ssh://localhost/home/svn/trunk@510 03485ba7-651f-0410-bc53-9ddc93d75666

--- src/te/Engine.java
+++ src/te/Engine.java
@@ -24,9 +24,9 @@ import org.mmbase.util.logging.*;
  */
 public class Engine implements Filter {
 
-    private static String engineName = "programma";
+    protected static String engineName = "programma";
     //every time a new EngineFacade is created a the counter is increased 
-    private static long creationCounter = 0;
+    protected static long creationCounter = 0;
 
     //	every time the EngineFacade is destroy is called a the counter is increased
     private static long destroyCounter = 0;
@@ -44,15 +44,16 @@ public class Engine implements Filter {
     private static Facade facade;
     
 	public Engine(){
-		System.err.println("LOG4J SUCKS");
 		
 	}
+	
     public void init(FilterConfig filterConfig) throws ServletException {
     	
         //this.filterConfig = filterConfig;
         creationCounter++;
         MMBaseContext.init(filterConfig.getServletContext());
-        MMBaseContext.initHtmlRoot();
+        MMBaseContext.initHtmlRoot() ;
+        MMBase.getMMBase();
         filterConfig.getServletContext();
         log = Logging.getLoggerInstance(Engine.class.getName() + "[" + creationCounter + "]");
         log.info("init of engine with name " + filterConfig.getFilterName());
@@ -68,109 +69,97 @@ public class Engine implements Filter {
     public void destroy() {
         destroyCounter++;
     }
-    /* (non-Javadoc)
-     * @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
-     */
-    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        try {
-
-            requestCounter++;
-
-            resp.setContentType("text/html;charset=" + Facade.encoding);
-            //resp.setContentType("text/html");
-
-            //code to determin the "engine url"             
-            if (facade.getEngineURL() == null) {
-                String servletPath = req.getRequestURI();
-                int match = servletPath.indexOf(engineName);
-                if (match > 0) {
-                    facade.setEngineURL(servletPath.substring(0, match + engineName.length() + 1));
-                    log.debug("setting engine url to" + facade.getEngineURL());
-                } else {
-                    log.warn("it looks like the engine is not called \"engine\" unable to determin the engine url");
-                }
-            }
-
-            //create your shared object
-            WhiteBoard wb = new WhiteBoard(req, resp);
-            wb.setFacade(facade);
-            //and store the whiteboard on it
-            req.setAttribute("wb", wb);
-
-            if (req.getRequestURI().length() == facade.getEngineURL().length()) {
-                return;
-            }
-
-            //extract the servlet context name and the eninge name
-            //this is the starting point
-            String path = req.getRequestURI().substring(facade.getEngineURL().length());
-
-            //remove leading slashes
-            while (path.charAt(path.length() - 1) == '/') {
-                path = path.substring(0, path.length() - 1);
-            }
-
-            if (log.isDebugEnabled()) {
-                //log.debug("request[" + requestCounter + "] servlet path info " + req.getPathInfo());
-                //log.debug("request[" + requestCounter + "] " + req.getRequestURI());
-                //log.debug("request[" + requestCounter + "] servlet request url " + req.getRequestURL());
-                //log.debug("request[" + requestCounter + "] servlet context path " + req.getContextPath());
-                //log.debug("request[" + requestCounter + "] servlet  path " + req.getServletPath());
-                //log.debug("request[" + requestCounter + "] guesed filter path " + facade.getEngineURL());
-                log.debug("request[" + requestCounter + "] requested " + path);
-            }
-
-            NavigationControl navigationComponent = facade.getNavigationControl();
-            //resolve the current navigation
-            Navigation nav = navigationComponent.resolveNavigation(new Path(path));
-            if (nav != null) {
-                //if the navigation was found put it in the whiteboard
-                wb.setCurrentNavigation(nav);
-                String navpath = navigationComponent.getURLString(nav);
-                log.debug("request[" + requestCounter + "] found " + navpath);
-
-                try {
-                    Template t = navigationComponent.getTemplate(nav);
-                    log.debug("using template: " + t.getName() + " " + t.getDescription());
-                    if (!path.equals(navpath)) {
-                        log.debug("navpath " + navpath);
-                        log.debug("path " + path);
-                        String remainingPath = path.substring(navpath.length() + NavigationControl.PATH_SEPARATOR.length());
-                        t.renderRelative(remainingPath, wb);
-                    } else {
-                        t.render(wb, resp.getWriter());
-                    }
-                } catch (IOException e) {
-                    log.warn(Logging.stackTrace(e));
-                } catch (Exception e) {
-                    log.warn(Logging.stackTrace(e));
-                }
-            } else {
-                try {
-                    req.getRequestDispatcher("/" + engineName + ".old/" + path).forward(req, resp);
-                    //filterChain.doFilter(servletRequest, servletResponse);
-
-                } catch (IOException e) {
-                    log.warn(Logging.stackTrace(e));
-                } catch (Exception e) {
-                    log.warn(Logging.stackTrace(e));
-                }
-            }
-            wb = null;
-        } catch (Throwable t) {
-            log.fatal(t.getMessage() + " " + Logging.stackTrace(t));
-        }
-    }
-
-    public void doPost(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
-        doGet(arg0, arg1);
-    }
 
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse resp = (HttpServletResponse) response;
-        doGet(req, resp);
+		try {
 
+			   requestCounter++;
+
+			   resp.setContentType("text/html;charset=" + Facade.encoding);
+			   //resp.setContentType("text/html");
+
+			   //code to determin the "engine url"             
+			   if (facade.getEngineURL() == null) {
+				   String servletPath = req.getRequestURI();
+				   int match = servletPath.indexOf(engineName);
+				   if (match > 0) {
+					   facade.setEngineURL(servletPath.substring(0, match + engineName.length() + 1));
+					   log.debug("setting engine url to" + facade.getEngineURL());
+				   } else {
+					   log.warn("it looks like the engine is not called \"engine\" unable to determin the engine url");
+				   }
+			   }
+
+			   //create your shared object
+			   WhiteBoard wb = new WhiteBoard(req, resp);
+			   wb.setFacade(facade);
+			   //and store the whiteboard on it
+			   req.setAttribute("wb", wb);
+
+			   if (req.getRequestURI().length() == facade.getEngineURL().length()) {
+				   return;
+			   }
+
+			   //extract the servlet context name and the eninge name
+			   //this is the starting point
+			   String path = req.getRequestURI().substring(facade.getEngineURL().length());
+
+			   //remove leading slashes
+			   while (path.charAt(path.length() - 1) == '/') {
+				   path = path.substring(0, path.length() - 1);
+			   }
+
+			   if (log.isDebugEnabled()) {
+				   //log.debug("request[" + requestCounter + "] servlet path info " + req.getPathInfo());
+				   //log.debug("request[" + requestCounter + "] " + req.getRequestURI());
+				   //log.debug("request[" + requestCounter + "] servlet request url " + req.getRequestURL());
+				   //log.debug("request[" + requestCounter + "] servlet context path " + req.getContextPath());
+				   //log.debug("request[" + requestCounter + "] servlet  path " + req.getServletPath());
+				   //log.debug("request[" + requestCounter + "] guesed filter path " + facade.getEngineURL());
+				   log.debug("request[" + requestCounter + "] requested " + path);
+			   }
+
+			   NavigationControl navigationComponent = facade.getNavigationControl();
+			   //resolve the current navigation
+			   Navigation nav = navigationComponent.resolveNavigation(new Path(path));
+			   if (nav != null) {
+				   //if the navigation was found put it in the whiteboard
+				   wb.setCurrentNavigation(nav);
+				   String navpath = navigationComponent.getURLString(nav);
+				   log.debug("request[" + requestCounter + "] found " + navpath);
+
+				   try {
+					   Template t = navigationComponent.getTemplate(nav);
+					   log.debug("using template: " + t.getName() + " " + t.getDescription());
+					   if (!path.equals(navpath)) {
+						   log.debug("navpath " + navpath);
+						   log.debug("path " + path);
+						   String remainingPath = path.substring(navpath.length() + NavigationControl.PATH_SEPARATOR.length());
+						   t.renderRelative(remainingPath, wb);
+					   } else {
+						   t.render(wb, resp.getWriter());
+					   }
+				   } catch (IOException e) {
+					   log.warn(Logging.stackTrace(e));
+				   } catch (Exception e) {
+					   log.warn(Logging.stackTrace(e));
+				   }
+			   } else {
+				   try {
+					   //req.getRequestDispatcher("/" + engineName + ".old/" + path).forward(req, resp);
+					   chain.doFilter(req, resp);
+				   } catch (IOException e) {
+					   log.warn(Logging.stackTrace(e));
+				   } catch (Exception e) {
+					   log.warn(Logging.stackTrace(e));
+				   }
+			   }
+			   wb = null;
+		   } catch (Throwable t) {
+			   log.fatal(t.getMessage() + " " + Logging.stackTrace(t));
+		   }
     }
 
 }
