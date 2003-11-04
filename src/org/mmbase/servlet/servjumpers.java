d9e4ebedd1652961439c90483cd8c6cbbabaa464
keesj <keesj@03485ba7-651f-0410-bc53-9ddc93d75666>
2003-11-04 09:54:29 +0000

    vpro version of servjumpers
    git-svn-id: svn+ssh://localhost/home/svn/trunk@517 03485ba7-651f-0410-bc53-9ddc93d75666

--- src/org/mmbase/servlet/servjumpers.java
+++ src/org/mmbase/servlet/servjumpers.java
@@ -19,6 +19,8 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import org.mmbase.util.logging.*;
 
+import org.mmbase.applications.te.*;
+
 /**
  * Servjumpers filters all url's to see if it has a jumper (specified in the
  * jumpers builder).
@@ -33,7 +35,11 @@ public class servjumpers extends JamesServlet {
     private static Logger log;
     // reference to the MMBase cloud
     static MMBase mmbase;
+    Engine engine;
+    public servjumpers() {
+        super();
 
+    }
     /**
      * tells if the user is using reload.
      *
@@ -41,49 +47,49 @@ public class servjumpers extends JamesServlet {
      * @return <code>true</code> if the user is using reloading, <code>false</code> otherwise.
      */
     private boolean reloadInfo(HttpServletRequest request) {
-        
-	    sessionsInterface sessions=null;
-	    Cookie[] cookies=request.getCookies();
 
-        if(cookies==null) {
+        sessionsInterface sessions = null;
+        Cookie[] cookies = request.getCookies();
+
+        if (cookies == null) {
             return false;
         }
         Cookie cookie = cookies[0];
-        
+
         // If user is not having a cookie he cannot be reloading.
-        if(cookie==null) {
+        if (cookie == null) {
             return false;
         }
-        
-        if (sessions==null) {
-            sessions = (sessionsInterface)mmbase.getModule("SESSION");
-            if(sessions==null) {
+
+        if (sessions == null) {
+            sessions = (sessionsInterface) mmbase.getModule("SESSION");
+            if (sessions == null) {
                 log.error("Need SESSION module to be able to retrieve the reload variable");
                 return false;
             }
         }
-        
+
         // Converting cookie to SCAN cookie
-        sessionInfo info = sessions.getSession(new scanpage(),"MMBase_Ident/"+cookie.getValue());
-        
+        sessionInfo info = sessions.getSession(new scanpage(), "MMBase_Ident/" + cookie.getValue());
+
         // if the reload variable is not set to R, the user is not reloading.
         String reload = sessions.getValue(info, "RELOAD");
-        if(reload==null || !reload.equals("R")) {
-            log.debug("reloading for user ("+cookie.getValue()+") is off");
+        if (reload == null || !reload.equals("R")) {
+            log.debug("reloading for user (" + cookie.getValue() + ") is off");
             return false;
         }
-        
+
         // Reloading is on, but check if the reload time is expired.
         String reloadtime = sessions.getValue(info, "RELOADTIME");
-        if (reloadtime!=null) {
-            int then=Integer.parseInt(reloadtime);
-            int now= (int)(DateSupport.currentTimeMillis()/1000);
-            if ((now-then)<EXPIRE) {
-                log.debug("reloading for user ("+cookie.getValue()+") is on");
+        if (reloadtime != null) {
+            int then = Integer.parseInt(reloadtime);
+            int now = (int) (DateSupport.currentTimeMillis() / 1000);
+            if ((now - then) < EXPIRE) {
+                log.debug("reloading for user (" + cookie.getValue() + ") is on");
                 return true;
             } else {
-                log.debug("reloading for user ("+cookie.getValue()+") is expired");
-                sessions.setValue(info, "RELOAD","N");
+                log.debug("reloading for user (" + cookie.getValue() + ") is expired");
+                sessions.setValue(info, "RELOAD", "N");
             }
         }
         return false;
@@ -94,7 +100,7 @@ public class servjumpers extends JamesServlet {
         // Initializing log here because log4j has to be initialized first.
         log = Logging.getLoggerInstance(servjumpers.class.getName());
         log.info("Init of servlet " + getServletConfig().getServletName() + ".");
-        mmbase=(MMBase)getModule("MMBASEROOT");
+        mmbase = (MMBase) getModule("MMBASEROOT");
     }
 
     /**
@@ -108,39 +114,43 @@ public class servjumpers extends JamesServlet {
      * @param req The HTTP Servlet request
      * @param res The HTTP Servlet response
      */
-    public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException,IOException {
-        incRefCount(req);  // increase reference count
+    public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        if (engine == null) {
+            engine = new Engine();
+        }
+        incRefCount(req); // increase reference count
         try {
-            String url=null;
-            String tmpr=req.getRequestURI().substring(1);
+            String url = null;
+            String tmpr = req.getRequestURI().substring(1);
 
-	    boolean reloadStatus=reloadInfo(req);
-	    log.debug("reloadStatus: "+reloadStatus);
+            boolean reloadStatus = reloadInfo(req);
+            log.debug("reloadStatus: " + reloadStatus);
 
             // jump.db was handled by the serdb servlet,
             // but it is nicer when it is handled in one place.
             // and the servdb had a little issue setting the mimetype for jumpers
             // which prevented some search bots to index the full site   
-        	if (tmpr.indexOf("jump.db") != -1 ) {
-        		String jumpKey = req.getQueryString();
-				if (jumpKey != null || "".equals(jumpKey.trim())) {        		
-        			url=getUrl(jumpKey,reloadStatus);
-				}
-        	} else {
-            	if (tmpr.indexOf('.')==-1 && (!tmpr.endsWith("/"))) {
-            	 	url=getUrl(tmpr,reloadStatus);
-            	}
-        	}
-            if (url!=null) {
+            if (tmpr.indexOf("jump.db") != -1) {
+                String jumpKey = req.getQueryString();
+                if (jumpKey != null || "".equals(jumpKey.trim())) {
+                    url = getUrl(jumpKey, reloadStatus);
+                }
+            } else {
+                if (tmpr.indexOf('.') == -1 && (!tmpr.endsWith("/"))) {
+                    url = getUrl(tmpr, reloadStatus);
+                }
+            }
+            if (url != null) {
                 res.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY); // 301
                 res.setContentType("text/html");
-                res.setHeader("Location",url);
+                res.setHeader("Location", url);
                 return; // ??
             }
-        }
-        finally { decRefCount(req); } // decrease reference count
+            engine.render(req, res);
+        } finally {
+            decRefCount(req);
+        } // decrease reference count
     }
-
 
     /**
      * Retrieve an alternate url based on a jumper key.
@@ -148,15 +158,16 @@ public class servjumpers extends JamesServlet {
      * @return the alternate yurl, or <code>null</code> if no url was found.
      */
     String getUrl(String key, boolean reloadStatus) {
-        String url=null;
-        Jumpers bul=(Jumpers)mmbase.getMMObject("jumpers");
-        if (bul!=null) {
+        String url = null;
+        Jumpers bul = (Jumpers) mmbase.getMMObject("jumpers");
+        if (bul != null) {
             if (key.endsWith("/")) {
-                url=bul.getJump(key.substring(0,key.length()-1),reloadStatus);
+                url = bul.getJump(key.substring(0, key.length() - 1), reloadStatus);
             } else {
-                url=bul.getJump(key,reloadStatus);
+                url = bul.getJump(key, reloadStatus);
             }
-            if (url!=null) return url;
+            if (url != null)
+                return url;
         } else {
             log.error("servjumpers -> can't access NodeManager jumpers (check jumpers.xml)");
         }
